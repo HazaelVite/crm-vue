4ab9096a6ad5f7abd69ad9386dfd7f5402ae53be
Hazael Vite <hvite@quologistics.com>
2023-10-11 12:49:38 -0600

    Consultando la api rest en vue

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { onMounted } from 'vue';
+import axios from 'axios';
 import RouterLink from "../components/general/RouterLink.vue";
 import Heading from "../components/general/Heading.vue";
+
+onMounted(() => {
+  // Con promesa
+  axios.get('http://localhost:4000/clientes')
+    .then(({ data }) => {
+      console.log(data);
+    }).catch(err => console.log('Error'))
+});
+
 </script>
 
 <template>
